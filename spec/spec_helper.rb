6d0761ac3a6310427121bfc458e651c7c0e61d2d
Brandon Raabe <brandocorp@gmail.com>
2015-12-25 20:26:38 -0700

    add code climate to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'simplecov'
-SimpleCov.start
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 
 require 'cocina'
 
